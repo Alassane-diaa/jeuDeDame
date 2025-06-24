cd4408d1e785be0839781b848708f6e5a2c01902
Abdoulaye Soumaré <abdoulayesilmansoum@gmail.com>
2025-06-24 09:42:33 +0200

    first commit

--- app/src/main/java/fr/minesnancy/diasoumare/jeudedames/jeuDeDames1.java
+++ app/src/main/java/fr/minesnancy/diasoumare/jeudedames/jeuDeDames1.java
@@ -1,12 +1,6 @@
 package fr.minesnancy.diasoumare.jeudedames;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-<<<<<<< HEAD
-=======
-import android.widget.Button;
->>>>>>> 3295585 (first commit)
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class jeuDeDames1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_jeu_de_dames1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void vsAI(View v){
-        Intent intent = new Intent(getApplicationContext(), VsAI.class);
-        startActivity(intent);
-    }
-
-    public void vsPlayer(View v){
-        Intent intent = new Intent(getApplicationContext(), VsPlayer.class);
-        startActivity(intent);
     }
 }
