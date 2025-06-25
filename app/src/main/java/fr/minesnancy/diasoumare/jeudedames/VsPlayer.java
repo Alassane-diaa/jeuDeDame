ca4c4e5d91744ba22b9bed9fd6626776575709c9
Alassane-diaa <dialassane19@gmail.com>
2025-06-25 20:09:33 +0200

    Ajout de quelques éléments graphiques simples - Commentaires pour Abdoulaye

--- app/src/main/java/fr/minesnancy/diasoumare/jeudedames/VsPlayer.java
+++ app/src/main/java/fr/minesnancy/diasoumare/jeudedames/VsPlayer.java
@@ -5,11 +5,13 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,8 +36,7 @@ public class VsPlayer extends AppCompatActivity {
 
     public void createGrid(){
         GridLayout layout = findViewById(R.id.grid);
-        int layoutSize = 35 * 20;
-        int screenSize = Resources.getSystem().getDisplayMetrics().widthPixels;
+        int layoutSize = 35 * 20; //350dp que j'ai mis en taille de la grille dans le fichier XML
         int boutonSize = layoutSize / 10;
         for(int i = 1; i <= 10; i++){
             for (int j = 1; j <= 10; j++){
@@ -55,8 +56,23 @@ public class VsPlayer extends AppCompatActivity {
                 boutonLayoutParams.width = boutonSize;
                 boutonLayoutParams.height = boutonSize;
                 bouton.setLayoutParams(boutonLayoutParams);
+                // Comme je t'avais dit, je crée juste les boutons, pas les listeners (c'est à dire ce qu'ils font quand on clique dessus)
+                // Il serait peut être pertinent de les mettre dans un tableau pour pouvoir les manipuler plus facilement
+                // Genre un tableau de 10x10, et chaque bouton serait à la position [i][j] dans le tableau
+                // Et tu pourrais repérer un bouton par son id en faisant bouton.setID(*) avec * un truc distinctif qui renseigne sur la couleur et la position
+                // Je me suis aussi rendu compte que je n'ai pas mis d'image pour le cas où une piece devient une dame
+                // Je vais faire ça après en fonction de la logique que tu veux implémenter
             }
         }
+    }
+    public void changeTurn(){
+        TextView textView = findViewById(R.id.turn);
+        if(textView.getText().toString().equals("Tour des blancs")){
+            textView.setText("Tour des noirs");
+        } else {
+            textView.setText("Tour des blancs");
+        }
+        // Là je change juste le texte, je te laisse activer et désactiver les boutons convenablement
     }
 
 }
